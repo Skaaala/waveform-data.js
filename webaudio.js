707a19b45c6b6dba952c95eaced6cfbd83a60498
Jan Skalický <skalicky.honza@gmail.com>
2019-08-27 10:18:19 +0200

    remove unused parts

--- webaudio.js
+++ webaudio.js
@@ -1,9 +1,5 @@
 "use strict";
 
 var webAudioBuilder = require("./lib/builders/webaudio");
-var fromAudioBuffer = require("./lib/builders/audiobuffer");
-
-
-webAudioBuilder.fromAudioBuffer = fromAudioBuffer;
 
 module.exports = webAudioBuilder;
